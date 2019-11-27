1c39edd511539c7ab98ade48d672c21c2fa3bc44
Owen Yaipen <1999yaipen@gmail.com>
2019-11-27 03:51:52 -0500

    favicon

--- resources/views/oauth/callback.blade.php
+++ resources/views/oauth/callback.blade.php
@@ -2,6 +2,8 @@
 <head>
   <meta charset="utf-8">
   <title>{{ config('app.name') }}</title>
+  <link rel="shortcut icon" href="{{ asset('img/pencil.png') }}"> 
+
   <script>
     window.opener.postMessage({ token: "{{ $token }}" }, "{{ url('/') }}")
     window.close()
